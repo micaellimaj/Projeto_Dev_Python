07951567408a42436fd2bdf1f918a13d43d7e98f
Micael Lima <micaeljose144@gmail.com>
2024-05-17 10:44:39 -0300

    adição de fig3 e fig4

--- datasets/dash/clientes.py
+++ datasets/dash/clientes.py
@@ -7,12 +7,7 @@ import plotly.express as px
 import dash
 from dash import dcc
 from dash import html 
-
-import dash
-from dash import dcc
-from dash import html
-import plotly.express as px
-import pandas as pd
+import plotly.graph_objs as go
 
 app = dash.Dash(__name__)
 
@@ -22,22 +17,59 @@ df = pd.read_csv(caminho_do_arquivo)
 # Primeiro gráfico
 fig1 = px.scatter(df, x="valor_compra(usd)", y="idade",
                   size="classificação_cliente_compra", color="temporada_compra", hover_name="temporada_compra",
-                  log_x=True, size_max=60)
+                  log_x=True, size_max=60, color_discrete_sequence=px.colors.qualitative.Plotly)
 
 # Segundo gráfico
 
-fig2 = px.box(df, x="método_pagamento", y="valor_compra(usd)",color="sexo")
+fig2 = px.box(df, x="método_pagamento", y="valor_compra(usd)",color="sexo", color_discrete_sequence=px.colors.qualitative.Prism)
+
+
 
 # Terceiro gráfico
+
+x = df['item_comprado']
+y = df['valor_compra(usd)']
+
+data = [go.Bar(
+            x=x,
+            y=y,
+            marker=dict(color=px.colors.qualitative.Pastel1)
+    )]
+layout = go.Layout(
+    title='Gráfico de Barras',
+    xaxis=dict(title='item_comprado'),
+    yaxis=dict(title='valor da compra (USD)')
+)
+
+fig3 = go.Figure(data=data, layout=layout)
+
+# quarto gráfico
+cores = {'Masculino': 'blue', 'Feminino': 'pink'}
+labels = df['sexo']
+values = df['valor_compra(usd)']
+
+fig4 = go.Figure(data=[go.Pie(labels=labels, values=values)])
+fig4.update_traces(marker=dict(colors=px.colors.qualitative.Dark2))
+
+# Configurando o layout
+fig4.update_layout(
+    title='valor da compra(USD) por sexo'
+)
 
 
 
 # Layout da aplicação com ambos os gráficos
 app.layout = html.Div([
-    dcc.Graph(id='valor-compra-vs-cliente', figure=fig1),
-    dcc.Graph(id='boxplot-metodo-pagamento', figure=fig2),
-    dcc.Graph(id='heatmap-tamanho-valorcompra', figure=fig3)
+    html.Div([
+        dcc.Graph(id='valor-compra-vs-cliente', figure=fig1),
+        dcc.Graph(id='boxplot-metodo-pagamento', figure=fig2),
+    ], style={'display': 'flex'}),
+    html.Div([
+        dcc.Graph(id='barras-sexo-valorcompra', figure=fig3),
+        dcc.Graph(id='pizza-sexo-valorcompra', figure=fig4)
+    ], style={'display': 'flex'})
 ])
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
