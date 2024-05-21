3374eddaa930db47e5d91f04e8aa7c3a9871acc8
Micael Lima <micaeljose144@gmail.com>
2024-05-21 19:25:41 -0300

    conteúdo desnecessário

--- components/sidebar.py
+++ components/sidebar.py
@@ -26,33 +26,7 @@ layout = dbc.Col([
                )
     
  ---------------------   # SEÇÃO NOVO ----------------------------------------------
-"""
-    dbc.Row([
-        dbc.Col([
-            dbc.button(color='sucess', id='open-novo-receita',
-                       children=['+Receita'])
-        ], width=6),
-            dbc.Col([
-                dbc.Button(color='danger', id='open-novo-despesa', 
-                           children=['-Despesas'])
-            ], width=6)
-        ])
 
-        # Modal Receita 
-        dbc.Modal([
-            dbc.ModalHeader(dbc.ModalTitle('Adicionar receita')),
-            dbc.ModalBody([
-
-            ])
-        ], id='modal-novo-receita'),
-        # Modal despesa
-        dbc.Modal([
-            dbc.ModalHeader(dbc.ModalTitle('Adicionar despesa')),
-            dbc.ModalBody([
-
-            ])
-        ], id='modal-novo-despesa'),
-        """
 
 # SEÇÃO NAV --------------------------------------------------
     html.Hr()
@@ -71,23 +45,3 @@ layout = dbc.Col([
 
 # pop-up receita
 
-"""
-@app.callback(
-    Output('modal-novo-receita','is_open'),
-    Input('open-novo-receita','n_clicks'),
-    State('modal-novo-receita', 'is_open')
-)
-def toggle_modal(n1, is_open):
-    if n1:
-        return not is_open
-    
-# pop-up despesa
-@app.callback(
-    Output('modal-novo-despesa','is_open'),
-    Input('open-novo-despesa','n_clicks'),
-    State('modal-novo-despesa', 'is_open')
-)
-def toggle_modal(n1, is_open):
-    if n1:
-        return not is_open
-    """
