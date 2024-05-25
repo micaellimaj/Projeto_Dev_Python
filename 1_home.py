c038b00855db1004fcab73ee6a0e4bd77596d454
Micael Lima <micaeljose144@gmail.com>
2024-05-24 21:11:43 -0300

    teste paginas

--- 1_home.py
+++ 1_home.py
@@ -7,24 +7,24 @@ import numpy as np
 # Importando Matplotlib para gráficos estáticos
 #import matplotlib.pyplot as plt
 
+
 # Importando Seaborn para gráficos mais sofisticados
 #import seaborn as sns
 
 #st.set_page_config(layout="wide")
 
 
-if "data" not in st.session_state:
-    df_data = pd.read_csv("datasets/df.csv")
-    st.session_state["data"] = df_data
+#if "data" not in st.session_state:
+df_data = pd.read_csv("datasets/df.csv")
+st.session_state["data"] = df_data
 
 st.write("Análise de dados")
 
 # página --------------------------------------------------
-
+st.sidebar.markdown("Desenvolvido por Estudantes Unifavip")
 
 
 # Lista de páginas disponíveis
-
 
 
 
@@ -32,3 +32,6 @@ st.write("Análise de dados")
 
 col1, col2 = st.columns(2)
 col3, col4, col5 = st.columns(3)
+
+fig_1 = px.bar(df_data, x = "item_comprado", y="valor_compra(usd)", title="valor comprado por item")
+col1.ploty_chart(fig_1, use_container_width=True)
--- pages/2_sales.py
+++ pages/2_sales.py
@@ -11,4 +11,16 @@ import numpy as np
 #import seaborn as sns
 
 
-st.session_state["data"]
+st.write("Análise de vendas")
+
+df_data = pd.read_csv("datasets/df.csv")
+st.session_state["data"] = df_data
+
+# Divisão da tela
+
+col1, col2 = st.columns(2)
+col3, col4, col5 = st.columns(3)
+
+# Fig 1 (Teste)
+fig_1 = px.bar(df_data, x = "item_comprado", y="valor_compra(usd)", title="valor comprado por item")
+col1.ploty_chart(fig_1, use_container_width=True)
